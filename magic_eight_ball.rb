61ca831e4a6d09a6c6b61ac27e0ec9660339baed
Eric Durr <emdurr@gmail.com>
2016-08-04 14:07:25 -0600

    next draft

--- magic_eight_ball.rb
+++ magic_eight_ball.rb
@@ -1,39 +1,77 @@
-#Welcome to Magic Eight Ball
-# What is your question? or type quit to end the program
-  # Set up if/else to handle input of quit
-# returns an answer for that question
-
 keep_looping = true
+
+@available_answers = [
+  "Not Likely",
+  "Unfortunately",
+  "Most Definitly",
+  "Why not?",
+  "You're up in the night",
+  "I don't have an opinion on that",
+  "Not in a million years!",
+  "You wish",
+  "Sure",
+  "Certainly!"
+]
 
 def opening_menu
   puts "\n-- Welcome to Magic Eight Ball!! --"
   puts "\n What is your question? or type QUIT to end program."
   user_input = gets.chomp.upcase
 
-  if user_input == 'QUIT'
-    puts "Thank you for playing!"
-    puts "Good-bye!!"
-    exit
-  else
-    answer
+  case user_input
+    when 'QUIT'
+      puts "Thank you for playing!"
+      puts "Good-bye!!"
+      exit
+    when 'WHAT IS THE BEST QUESTION?'
+      puts "Excellent question!!"
+      easter_egg_1
+    when 'WILL I EVER FEEL YOUNG AGAIN?'
+      puts "That's the perfect question!"
+      easter_egg_2
+    when 'WHERE DO I GO FROM HERE?'
+      puts "How did you know my secret?!"
+      easter_egg_3
+    else
+      answer
   end
 end
 
+def easter_egg_1
+  puts "What answer would you like to add?"
+  user_input_2 = gets.strip
+
+  if @available_answers2.include?(user_input_2)
+    puts "I'm sorry, that answer already exists."
+    puts "Thanks for trying, though"
+    opening_menu
+  else
+    puts "Thank you! #{user_input_2} added."
+    @available_answers2 << user_input_2
+  end
+end
+
+def easter_egg_2
+  puts "Reset available answers? y/n"
+  input = gets.strip.downcase
+  if input == 'y'
+    @available_answers2.clear
+    puts "\n"
+    arr_clone
+  else
+    puts "Thanks anyway."
+    opening_menu
+  end
+end
+
+def arr_clone
+  @available_answers2 = @available_answers.clone
+  opening_menu
+end
+
 def answer
-  available_answers = [
-    "Not Likely",
-    "Unfortunately",
-    "Most Definitly",
-    "Why not?",
-    "You're up in the night",
-    "I don't have an opinion on that",
-    "Not in a million years!",
-    "You wish",
-    "Sure",
-    "Certainly!"
-  ]
   puts "The Magic Eight Ball says:"
-  puts available_answers.sample
+  puts @available_answers2.sample
 end
 
 while keep_looping == true
