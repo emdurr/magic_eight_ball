9c96e21d7d2296078009adb2ea328f1dee4fe947
Eric Durr <emdurr@gmail.com>
2016-08-04 15:18:08 -0600

    all bonus material works

--- magic_eight_ball.rb
+++ magic_eight_ball.rb
@@ -1,19 +1,22 @@
 keep_looping = true
 
-@available_answers = [
-  "Not Likely",
-  "Unfortunately",
-  "Most Definitly",
-  "Why not?",
-  "You're up in the night",
-  "I don't have an opinion on that",
-  "Not in a million years!",
-  "You wish",
-  "Sure",
-  "Certainly!"
-]
+@available_answers = []
+
 
 def opening_menu
+  if @available_answers.empty?
+    @available_answers.push("Not Likely",
+    "Unfortunately",
+    "Most Definitly",
+    "Why not?",
+    "You're up in the night",
+    "I don't have an opinion on that",
+    "Not in a million years!",
+    "You wish",
+    "Sure",
+    "Certainly!")
+  else
+  end
   puts "\n-- Welcome to Magic Eight Ball!! --"
   puts "\n What is your question? or type QUIT to end program."
   user_input = gets.chomp.upcase
@@ -23,55 +26,54 @@ def opening_menu
       puts "Thank you for playing!"
       puts "Good-bye!!"
       exit
-    when 'WHAT IS THE BEST QUESTION?'
+    when 'WHY DO YOU ONLY HAVE A LIMITED ANSWER SELECTION?'
       puts "Excellent question!!"
-      easter_egg_1
+      add_answer
     when 'WILL I EVER FEEL YOUNG AGAIN?'
       puts "That's the perfect question!"
-      easter_egg_2
+      reset_answers
     when 'WHERE DO I GO FROM HERE?'
       puts "How did you know my secret?!"
-      easter_egg_3
+      print_answers
     else
       answer
   end
 end
 
-def easter_egg_1
+def add_answer
   puts "What answer would you like to add?"
   user_input_2 = gets.strip
 
-  if @available_answers2.include?(user_input_2)
+  if @available_answers.include?(user_input_2)
     puts "I'm sorry, that answer already exists."
     puts "Thanks for trying, though"
     opening_menu
   else
     puts "Thank you! #{user_input_2} added."
-    @available_answers2 << user_input_2
+    @available_answers << user_input_2
   end
 end
 
-def easter_egg_2
+def reset_answers
   puts "Reset available answers? y/n"
   input = gets.strip.downcase
   if input == 'y'
-    @available_answers2.clear
+    @available_answers.clear
     puts "\n"
-    arr_clone
+    opening_menu
   else
     puts "Thanks anyway."
     opening_menu
   end
 end
 
-def arr_clone
-  @available_answers2 = @available_answers.clone
-  opening_menu
+def print_answers
+  puts @available_answers.join("\n")
 end
 
 def answer
   puts "The Magic Eight Ball says:"
-  puts @available_answers2.sample
+  puts @available_answers.sample
 end
 
 while keep_looping == true
